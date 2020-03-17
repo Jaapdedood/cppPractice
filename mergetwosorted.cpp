82bd38f043d52e9598b3e347bf0d90926d336e96
Jaapdedood <jaapdd@gmail.com>
2020-03-16 22:53:25 -0700

    did mergetwosorted

--- mergetwosorted.cpp
+++ mergetwosorted.cpp
@@ -23,22 +23,49 @@ void addNode(ListNode **head, int n){
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* result;
-        return result;
+        ListNode* temp = new ListNode(-1);
+        ListNode* result = temp;
+
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val < l2->val){
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+
+        if(l1 != nullptr){
+            temp->next = l1;
+        }
+        else{
+            temp->next = l2;
+        }
+
+        return result->next;
     }
 };
 
 int main(){
-    ListNode* head = new ListNode(4);
-    addNode(&head, 2);
-    addNode(&head, 1);
+    Solution solution;
+
+    ListNode* head1 = new ListNode(4);
+    addNode(&head1, 2);
+    addNode(&head1, 1);
     
     ListNode* head2 = new ListNode(4);
     addNode(&head2, 3);
     addNode(&head2, 1);
 
-    printList(head);
+    printList(head1);
     printList(head2);
+
+    ListNode* merged = solution.mergeTwoLists(head1, head2);
+
+    printList(merged);
 
     return 0;
 }
